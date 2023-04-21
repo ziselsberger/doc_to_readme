57962bbd987e312849422db525f0bc4c328a4df9
Mirjam Ziselsberger <ziselsberger@geoville.com>
2023-04-21 11:16:06 +0200

    feat: update test.py
    - update imports
    - remove TestCase ExtractInfo
    - update TestCase LoopThroughRepo

--- tests/test.py
+++ tests/test.py
@@ -1,34 +1,16 @@
 import unittest
 
-import doc_to_readme.src.doc_to_md as dtm
-import doc_to_readme.src.functions as f
-
-
-class ExtractInfo(unittest.TestCase):
-    def setUp(self) -> None:
-        dtm.docu = ""
-        self.updated_docu = \
-            "\n* ### `mean(x: int = 1, y: int = 2) -> float`  \n" \
-            "      Calculate mean of x and y.\n"
-        self.extracted_info = (
-            'mean',
-            'mean(x: int = 1, y: int = 2) -> float',
-            'Calculate mean of x and y.'
-        )
-
-    def test_doc_to_md(self):
-        extract = dtm.doc_to_md(f.mean)
-        self.assertEqual(extract, self.extracted_info)
-        self.assertEqual(dtm.docu, self.updated_docu)
+import doc_to_readme.src.doc_to_md.doc_to_md as dtm
 
 
 class LoopThroughRepo(unittest.TestCase):
     def setUp(self) -> None:
         dtm.summary = {}
-        self.module = "functions"
+        self.file = "./TEST_README.md"
+        self.module = "functions_for_testing"
         self.function = "add"
-        self.remaining_modules = ['main', 'doc_to_md', 'run_qc_checks', 'test']
-        self.correct_link = '[functions](.\\src\\functions.py)'
+        self.remaining_modules = ['classes_for_testing', 'test']
+        self.correct_link = '[functions_for_testing](.\\functions_for_testing.py)'
         self.correct_dict = {
             'doc': 'Add two numbers (x and y).',
             'fn': 'add(x: int = 4, y: int = 5) -> int',
@@ -37,7 +19,7 @@ class LoopThroughRepo(unittest.TestCase):
         }
 
     def test_loop_through_repo(self):
-        dtm.loop_through_repo()
+        dtm.loop_through_repo(file=self.file)
         self.assertEqual(
             dtm.summary[self.module]['Link'],
             self.correct_link
@@ -48,7 +30,10 @@ class LoopThroughRepo(unittest.TestCase):
         )
 
     def test_loop_through_repo_specified_modules(self):
-        dtm.loop_through_repo(specified_modules=(self.module,))
+        dtm.loop_through_repo(
+            file=self.file,
+            specified_modules=(self.module,)
+        )
         self.assertEqual(
             dtm.summary[self.module]['Link'],
             self.correct_link
@@ -59,7 +44,10 @@ class LoopThroughRepo(unittest.TestCase):
         )
 
     def test_loop_through_repo_exclude_modules(self):
-        dtm.loop_through_repo(exclude_modules=(self.module,))
+        dtm.loop_through_repo(
+            file=self.file,
+            exclude_modules=(self.module,)
+        )
         with self.assertRaises(KeyError):
             print(dtm.summary[self.module])
         self.assertListEqual(
